6e6c84087f20afdf2391230aa6a7ab20c1e148e9
Manuel Hernandez <mandrodiaz@gmail.com>
2024-05-04 19:44:59 -0700

    cambios maestros.php

--- layout/sidebar-template.php
+++ layout/sidebar-template.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="./index.html" class="brand-link">
-      <img src="./src/images/v2_4.png" alt="AdminLTE Logo" class="img-thumbnail" style="opacity: .8">
+    <a href="<?=BASE_PATH?>" class="brand-link">
+      <img src="<?=BASE_PATH?>src/images/v2_4.png" alt="AdminLTE Logo" class="img-thumbnail" style="opacity: .8">
     </a>
 
     <!-- Sidebar -->
@@ -9,7 +9,7 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="<?=BASE_PATH?>dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block">Administrador</a>
@@ -20,38 +20,38 @@
       <nav class="mt-2">
         <ul  class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <li class="nav-item">
-            <a href="#" class="nav-link d-flex justify-content-start align-items-center">
-              <img src="src/images/sidebar/home-outline.svg" class="mr-1" alt="">
+            <a href="<?=BASE_PATH?>" class="nav-link d-flex justify-content-start align-items-center">
+              <img src="<?=BASE_PATH?>src/images/sidebar/home-outline.svg" class="mr-1" alt="">
               <p>Inicio</p>
             </a>
           </li>
           <li class="nav-item">
             <a href="#" class="nav-link d-flex justify-content-start align-items-center">
-              <img src="src/images/sidebar/people-outline.svg" alt="">
+              <img src="<?=BASE_PATH?>src/images/sidebar/people-outline.svg" alt="">
               <p>Alumnos</p>
             </a>
           </li>
           <li class="nav-item">
-            <a href="#" class="nav-link d-flex justify-content-start align-items-center">
-              <img src="src/images/sidebar/barbell-outline.svg" alt="">
+            <a href="<?=BASE_PATH?>routes/maestros.php" class="nav-link d-flex justify-content-start align-items-center">
+              <img src="<?=BASE_PATH?>src/images/sidebar/barbell-outline.svg" alt="">
               <p>Maestros</p>
             </a>
           </li>
           <li class="nav-item">
             <a href="#" class="nav-link d-flex justify-content-start align-items-center">
-              <img src="src/images/sidebar/card-outline.svg" alt="">
+              <img src="<?=BASE_PATH?>src/images/sidebar/card-outline.svg" alt="">
               <p>Pagos</p>
             </a>
           </li>
           <li class="nav-item">
             <a href="#" class="nav-link d-flex justify-content-start align-items-center">
-              <img src="src/images/sidebar/newspaper-outline.svg" alt="">
+              <img src="<?=BASE_PATH?>src/images/sidebar/newspaper-outline.svg" alt="">
               <p>Anuncios</p>
             </a>
           </li>
           <li class="nav-item">
             <a href="#" class="nav-link d-flex justify-content-start align-items-center">
-              <img src="src/images/sidebar/cog-outline.svg" alt="">
+              <img src="<?=BASE_PATH?>src/images/sidebar/cog-outline.svg" alt="">
               <p>Ajustes</p>
             </a>
           </li>
--- routes/maestros.php
+++ routes/maestros.php
@@ -12,99 +12,16 @@
 <body class="hold-transition sidebar-mini">
 <div class="wrapper">
 
-  <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-    <!-- Left navbar links -->
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="index3.html" class="nav-link">Home</a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">Contact</a>
-      </li>
-    </ul>
-  </nav>
-  <!-- /.navbar -->
-
+  
   <!-- Main Sidebar Container -->
-  <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
-    </a>
+  
+  <?php
+    include '../layout/sidebar-template.php';
+  ?>
 
-    <!-- Sidebar -->
-    <div class="sidebar">
-      <!-- Sidebar user panel (optional) -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-        </div>
-        <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
-        </div>
-      </div>
-
-      <!-- SidebarSearch Form -->
-      <div class="form-inline">
-        <div class="input-group" data-widget="sidebar-search">
-          <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
-          <div class="input-group-append">
-            <button class="btn btn-sidebar">
-              <i class="fas fa-search fa-fw"></i>
-            </button>
-          </div>
-        </div>
-      </div>
-
-      <!-- Sidebar Menu -->
-      <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
-          <li class="nav-item menu-open">
-            <a href="#" class="nav-link active">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
-              <p>
-                Starter Pages
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="#" class="nav-link active">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Active Page</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Inactive Page</p>
-                </a>
-              </li>
-            </ul>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-th"></i>
-              <p>
-                Simple Link
-                <span class="right badge badge-danger">New</span>
-              </p>
-            </a>
-          </li>
-        </ul>
-      </nav>
-      <!-- /.sidebar-menu -->
-    </div>
-    <!-- /.sidebar -->
-  </aside>
-
+  <?php
+    include '../layout/navbar-template.php';
+  ?>
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
@@ -112,14 +29,9 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0">Starter Page</h1>
+            <h1 class="m-0">Maestros</h1>
           </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Starter Page</li>
-            </ol>
-          </div><!-- /.col -->
+          
         </div><!-- /.row -->
       </div><!-- /.container-fluid -->
     </div>
@@ -129,69 +41,95 @@
     <div class="content">
       <div class="container-fluid">
         <div class="row">
-          <div class="col-lg-6">
-            <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up the bulk of the card's
-                  content.
-                </p>
-
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
-              </div>
+          <div class="col-lg-12 mt-5">
+            <div class="col-lg-12 d-flex justify-content-end mb-4">
+              <button class="btn btn-primary">Agregar maestro</button>
             </div>
-
-            <div class="card card-primary card-outline">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up the bulk of the card's
-                  content.
-                </p>
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
+            <div class="card pt-5">
+              <!-- /.card-header -->
+              <div class="card-body p-5 ">
+                <ul class="users-list clearfix">
+                  <li>
+                    <img src="<?=BASE_PATH?>dist/img/user1-128x128.jpg" alt="User Image">
+                    <a class="users-list-name" href="#">Alexander Pierce</a>
+                    <span class="users-list-date">Today</span>
+                  </li>
+                  <li>
+                    <img src="<?=BASE_PATH?>dist/img/user1-128x128.jpg" alt="User Image">
+                    <a class="users-list-name" href="#">Alexander Pierce</a>
+                    <span class="users-list-date">Today</span>
+                  </li>
+                  <li>
+                    <img src="<?=BASE_PATH?>dist/img/user1-128x128.jpg" alt="User Image">
+                    <a class="users-list-name" href="#">Alexander Pierce</a>
+                    <span class="users-list-date">Today</span>
+                  </li>
+                  <li>
+                    <img src="<?=BASE_PATH?>dist/img/user1-128x128.jpg" alt="User Image">
+                    <a class="users-list-name" href="#">Alexander Pierce</a>
+                    <span class="users-list-date">Today</span>
+                  </li>
+                  <li>
+                    <img src="<?=BASE_PATH?>dist/img/user1-128x128.jpg" alt="User Image">
+                    <a class="users-list-name" href="#">Alexander Pierce</a>
+                    <span class="users-list-date">Today</span>
+                  </li>
+                  <li>
+                    <img src="<?=BASE_PATH?>dist/img/user1-128x128.jpg" alt="User Image">
+                    <a class="users-list-name" href="#">Alexander Pierce</a>
+                    <span class="users-list-date">Today</span>
+                  </li>
+                  <li>
+                    <img src="<?=BASE_PATH?>dist/img/user1-128x128.jpg" alt="User Image">
+                    <a class="users-list-name" href="#">Alexander Pierce</a>
+                    <span class="users-list-date">Today</span>
+                  </li>
+                  <li>
+                    <img src="<?=BASE_PATH?>dist/img/user1-128x128.jpg" alt="User Image">
+                    <a class="users-list-name" href="#">Alexander Pierce</a>
+                    <span class="users-list-date">Today</span>
+                  </li>
+                 
+                </ul>
+                <!-- /.users-list -->
               </div>
-            </div><!-- /.card -->
+              <!-- /.card-body -->
+              <div class="card-footer text-center">
+                <a href="javascript:">View All Users</a>
+              </div>
+              <!-- /.card-footer -->
+            </div>
           </div>
-          <!-- /.col-md-6 -->
-          <div class="col-lg-6">
-            <div class="card">
-              <div class="card-header">
-                <h5 class="m-0">Featured</h5>
-              </div>
-              <div class="card-body">
-                <h6 class="card-title">Special title treatment</h6>
-
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-
-            <div class="card card-primary card-outline">
-              <div class="card-header">
-                <h5 class="m-0">Featured</h5>
-              </div>
-              <div class="card-body">
-                <h6 class="card-title">Special title treatment</h6>
-
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-          </div>
-          <!-- /.col-md-6 -->
         </div>
         <!-- /.row -->
-      </div><!-- /.container-fluid -->
+        <div class="card bg-gradient-primary" style="visibility:hidden">
+          <div class="card-footer bg-transparent">
+            <div class="row">
+              <div class="col-4 text-center">
+                <div id="sparkline-1"><canvas width="80" height="50" style="width: 80px; height: 50px;"></canvas></div>
+                <div class="text-white">Visitors</div>
+              </div>
+              <!-- ./col -->
+              <div class="col-4 text-center">
+                <div id="sparkline-2"><canvas width="80" height="50" style="width: 80px; height: 50px;"></canvas></div>
+                <div class="text-white">Online</div>
+              </div>
+              <!-- ./col -->
+              <div class="col-4 text-center">
+                <div id="sparkline-3"><canvas width="80" height="50" style="width: 80px; height: 50px;"></canvas></div>
+                <div class="text-white">Sales</div>
+              </div>
+              <!-- ./col -->
+            </div>
+          </div><!-- /.container-fluid -->
+        </div>
+      <!-- /.row -->
     </div>
+  </div>
     <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
-
+  
   <!-- Control Sidebar -->
   <aside class="control-sidebar control-sidebar-dark">
     <!-- Control sidebar content goes here -->
@@ -200,17 +138,16 @@
       <p>Sidebar content</p>
     </div>
   </aside>
-  
+  <?php
+    include '../layout/footer-template.php';
+  ?>
 </div>
 <!-- ./wrapper -->
 
 <!-- REQUIRED SCRIPTS -->
 
-<!-- jQuery -->
-<script src="plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="dist/js/adminlte.min.js"></script>
+<?php
+  include '../layout/scripts-template.php';
+?>
 </body>
 </html>
