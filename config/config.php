643432bb749ccd3d124b7af39a1d1d2d152d8a7e
Manuel Hernandez <mandrodiaz@gmail.com>
2024-05-04 19:01:57 -0700

    Limpieza del directorio para empezar a maquetar

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+
+if(!defined('BASE_PATH')){
+    define('BASE_PATH','http://localhost/gymnastika/');
+}
+?>
---
+++ layout/sidebar-template.php
@@ -0,0 +1,65 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="./index.html" class="brand-link">
+      <img src="./src/images/v2_4.png" alt="AdminLTE Logo" class="img-thumbnail" style="opacity: .8">
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block">Administrador</a>
+        </div>
+      </div>
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul  class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <li class="nav-item">
+            <a href="#" class="nav-link d-flex justify-content-start align-items-center">
+              <img src="src/images/sidebar/home-outline.svg" class="mr-1" alt="">
+              <p>Inicio</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link d-flex justify-content-start align-items-center">
+              <img src="src/images/sidebar/people-outline.svg" alt="">
+              <p>Alumnos</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link d-flex justify-content-start align-items-center">
+              <img src="src/images/sidebar/barbell-outline.svg" alt="">
+              <p>Maestros</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link d-flex justify-content-start align-items-center">
+              <img src="src/images/sidebar/card-outline.svg" alt="">
+              <p>Pagos</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link d-flex justify-content-start align-items-center">
+              <img src="src/images/sidebar/newspaper-outline.svg" alt="">
+              <p>Anuncios</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link d-flex justify-content-start align-items-center">
+              <img src="src/images/sidebar/cog-outline.svg" alt="">
+              <p>Ajustes</p>
+            </a>
+          </li>
+          
+          
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
