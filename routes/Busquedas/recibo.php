fc5af9578701eb37bfed3664af4fddf4f180c58f
Oscar Gutierrez <Og7240014@gmail.com>
2024-05-21 00:49:09 -0700

    gg

--- routes/Busquedas/recibo.php
+++ routes/Busquedas/recibo.php
@@ -49,7 +49,7 @@ ob_start();
               <p>( )  Efectivo</p> 
             </div>
             <br>
-            <div class= "row text-left">
+            <div class= "row text-left ml-2">
               <p>Recibido: </p>
               <img src="http://localhost/gymnastika/src/images/Firma.png" width="70px" alt="Logo del gimnasio"> 
             </div>
@@ -61,43 +61,6 @@ ob_start();
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
   </body>
 </html>
-<!-- <div class="container">
-        <div class="logo">
-            <img src="http://localhost/gymnastika/src/images/v2_4.png" width="100px" alt="Logo del gimnasio">
-        </div>
-        <h2 class="text-center">Datos de Alumnos</h2>
-        <div class="table-responsive">
-            <table id="example" class="table table-striped table-bordered" style="width:100%">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Nombre</th>
-                        <th>Fecha de Ingreso</th>
-                        <th>CURP</th>
-                        <th>Información de Padre</th>
-                        <th>Información de Madre</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    // if ($result->num_rows > 0) {
-                    //     while($row = $result->fetch_assoc()) {
-                    //         echo "<tr>";
-                    //         echo "<td>" . $row["id_alumno"]. "</td>";
-                    //         echo "<td>" . $row["nombres"]. " " . $row["apellidos"]. "</td>";
-                    //         echo "<td>" . $row["fecha_ingreso"]. "</td>";
-                    //         echo "<td>" . $row["curp"]. "</td>";
-                    //         echo "<td>" . $row["info_padre_id"]. "</td>";
-                    //         echo "<td>" . $row["info_madre_id"]. "</td>";
-                    //         echo "</tr>";
-                    //     }
-                    // } 
-                    ?>
-                </tbody>
-            </table>
-        </div>
-    </div> -->
-
 <?php
 $html = ob_get_clean();
 require '../../vendor/autoload.php';
@@ -107,7 +70,7 @@ $dompdf = new Dompdf();
 
 $options = $dompdf->getOptions(); $options->set(array('isRemoteEnabled'=>
 true)); $dompdf->setOptions($options); 
-$html .= '<style> @page { size: 8.5in 6.0in; margin: 0.3in; } </style>';
+$html .= '<style> @page { size: 8.5in 7.0in; margin: 0.3in; } </style>';
 $dompdf->loadHtml($html);
 
 $dompdf->render();
