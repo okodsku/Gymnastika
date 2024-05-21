3994243846891725e238a9c9a290dd66795128e0
Oscar Gutierrez <Og7240014@gmail.com>
2024-05-20 19:28:10 -0700

    cambio de administrador

--- layout/sidebar-template.php
+++ layout/sidebar-template.php
@@ -9,10 +9,10 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="<?=BASE_PATH?>dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="<?=BASE_PATH?>src/images/v2_9.PNG" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Administrador</a>
+          <a>Gina</a>
         </div>
       </div>
 
